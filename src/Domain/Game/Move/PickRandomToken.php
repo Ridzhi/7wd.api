ed3b7c1b4d3408b2b4fa1ad53d9cc5bdc07ce565
Danila Stivrinsh <danilaridzhi@gmail.com>
2022-01-03 20:26:03 +0300

    upd

--- src/Domain/Game/Move/PickRandomToken.php
+++ src/Domain/Game/Move/PickRandomToken.php
@@ -3,8 +3,10 @@
 namespace App\Domain\Game\Move;
 
 use App\Domain\Game\MutatorInterface;
+use App\Domain\Game\Phase;
 use App\Domain\Game\State\State;
 use App\Domain\Game\Token\Id as Tid;
+use App\Domain\Game\Token\Repository as TokenRepository;
 
 class PickRandomToken implements MutatorInterface
 {
@@ -18,6 +20,17 @@ class PickRandomToken implements MutatorInterface
 
     public function mutate(State $state): void
     {
-        // TODO: Implement mutate() method.
+        if ($state->phase !== Phase::PickRandomToken) {
+            throw new InvalidError();
+        }
+
+        if (!in_array($this->token, $state->dialogItems->tokens)) {
+            throw new InvalidError();
+        }
+
+        $state->me->tokens->add($this->token);
+        TokenRepository::get($this->token)->mutate($state);
+
+        $state->after();
     }
 }
