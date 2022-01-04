a1f7c2b8514bb6e65fa19ff7a8faa4606dbb04c4
Danila Stivrinsh <danilaridzhi@gmail.com>
2022-01-04 17:28:29 +0300

    upd

--- src/Domain/Game/Effect/Resource.php
+++ src/Domain/Game/Effect/Resource.php
@@ -2,12 +2,13 @@
 
 namespace App\Domain\Game\Effect;
 
+use App\Domain\Game\BurnInterface;
 use App\Domain\Game\MutatorInterface;
 use App\Domain\Game\Resource\Id as Rid;
 use App\Domain\Game\Rule;
 use App\Domain\Game\State\State;
 
-class Resource implements MutatorInterface
+class Resource implements MutatorInterface, BurnInterface
 {
     public Id $id = Id::Resource;
 
@@ -25,6 +26,16 @@ class Resource implements MutatorInterface
         if (!$state->enemy->bank->hasFixedResourcePrice($this->resource)) {
             $newPrice = Rule::DEFAULT_RESOURCE_PRICE + $state->me->resources[$this->resource];
             $state->enemy->bank->resourcePrice[$this->resource] = $newPrice;
+        }
+    }
+
+    public function burn(State $state): void
+    {
+        $state->enemy->resources[$this->resource] -= $this->count;
+
+        if (!$state->me->bank->hasFixedResourcePrice($this->resource)) {
+            $newPrice = Rule::DEFAULT_RESOURCE_PRICE + $state->enemy->resources[$this->resource];
+            $state->me->bank->resourcePrice[$this->resource] = $newPrice;
         }
     }
 }
