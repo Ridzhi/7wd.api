bf95645e9bec136ddf6a2fba5cd6c324e72d4f63
Danila Stivrinsh <danilaridzhi@gmail.com>
2022-01-04 17:32:27 +0300

    upd

--- src/Domain/Game/Effect/Science.php
+++ src/Domain/Game/Effect/Science.php
@@ -5,28 +5,30 @@ namespace App\Domain\Game\Effect;
 use App\Domain\Game\MutatorInterface;
 use App\Domain\Game\State\State;
 use App\Domain\Game\Symbol;
+use App\Domain\Game\SymbolStatus;
+use App\Domain\Game\Victory;
 
-class Science extends Base implements MutatorInterface
+class Science implements MutatorInterface
 {
+    public Id $id = Id::Science;
+
     public function __construct(
         public readonly Symbol $symbol,
     )
     {
-        parent::__construct(Id::Science);
     }
 
     public function mutate(State $state): void
     {
-//        $state->me->symbols->add($this->symbol);
-//
-//        if ($tokenReward = $state->me->symbols->count($this->symbol) === Value::TOKEN_SYMBOLS_COUNT) {
-//            $state->apply(new PickToken(new BoardTokens()));
-//
-//            return;
-//        }
-//
-//        if ($supremacyReached = $state->me->symbols->count() === Value::SUPREMACY_TOKENS_COUNT) {
-//            $state->victory = Victory::Science;
-//        }
+        switch ($state->me->symbols->add($this->symbol)) {
+            case SymbolStatus::Token:
+                (new PickBoardToken())->mutate($state);
+                break;
+            case SymbolStatus::Supremacy:
+                $state->over(Victory::Science, $state->me->name);
+                break;
+            case SymbolStatus::Null:
+                //nothing
+        }
     }
 }
