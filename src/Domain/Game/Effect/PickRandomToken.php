d9a01aed3cd1a5cba92fff74be7af31b90dcd8a1
Danila Stivrinsh <danilaridzhi@gmail.com>
2022-01-04 17:01:44 +0300

    upd

--- src/Domain/Game/Effect/PickRandomToken.php
+++ src/Domain/Game/Effect/PickRandomToken.php
@@ -5,15 +5,12 @@ namespace App\Domain\Game\Effect;
 use App\Domain\Game\MutatorInterface;
 use App\Domain\Game\State\State;
 
-class PickRandomToken extends Base implements MutatorInterface
+class PickRandomToken implements MutatorInterface
 {
-    public function __construct()
-    {
-        parent::__construct(Id::PickRandomToken);
-    }
+    public Id $id = Id::PickRandomToken;
 
     public function mutate(State $state): void
     {
-        // TODO: Implement mutate() method.
+        // @TODO dialog
     }
 }
