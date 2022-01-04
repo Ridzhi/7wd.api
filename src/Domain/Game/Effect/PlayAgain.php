ba605e32fd24a963244449cb3b3e5a4fe35cb499
Danila Stivrinsh <danilaridzhi@gmail.com>
2022-01-04 17:03:26 +0300

    upd

--- src/Domain/Game/Effect/PlayAgain.php
+++ src/Domain/Game/Effect/PlayAgain.php
@@ -5,15 +5,12 @@ namespace App\Domain\Game\Effect;
 use App\Domain\Game\MutatorInterface;
 use App\Domain\Game\State\State;
 
-class PlayAgain extends Base implements MutatorInterface
+class PlayAgain implements MutatorInterface
 {
-    public function __construct()
-    {
-        parent::__construct(Id::PlayAgain);
-    }
+    public Id $id = Id::PlayAgain;
 
     public function mutate(State $state): void
     {
-        // TODO: Implement mutate() method.
+        $state->playAgain = true;
     }
 }
