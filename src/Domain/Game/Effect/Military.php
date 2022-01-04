1fe2eceaa621deb66ec6d307f259fb1e147b1c56
Danila Stivrinsh <danilaridzhi@gmail.com>
2022-01-04 16:54:38 +0300

    upd

--- src/Domain/Game/Effect/Military.php
+++ src/Domain/Game/Effect/Military.php
@@ -4,35 +4,44 @@ namespace App\Domain\Game\Effect;
 
 use App\Domain\Game\MutatorInterface;
 use App\Domain\Game\State\State;
+use App\Domain\Game\Token\Id as Tid;
+use App\Domain\Game\Victory;
 use Symfony\Component\Serializer\Annotation\Ignore;
 
-class Military extends Base implements MutatorInterface
+class Military implements MutatorInterface
 {
+    public Id $id = Id::Military;
+
     public function __construct(
-        public readonly int  $power,
+        public readonly int $power,
         #[Ignore]
         public readonly bool $strategyAllowed = true,
     )
     {
-        parent::__construct(Id::Military);
     }
 
     public function mutate(State $state): void
     {
-//        $power = $this->power;
-//
-//        if ($this->strategyAllowed && $state->me->tokens->has(Tid::Strategy)) {
-//            $power++;
-//        }
-//
-//        [$fine, $supremacy] = $state->board->moveConflictPawn($state->me->name, $power);
-//
-//        if ($fine > 0) {
-//            $state->enemy->treasure->add(-$fine);
-//        }
-//
-//        if ($supremacy) {
-//            $state->victory = Victory::Military;
-//        }
+        $power = $this->power;
+
+        if (
+            $this->strategyAllowed
+            && $state->me->tokens->has(Tid::Strategy)
+        ) {
+            $power++;
+        }
+
+        [
+            'fine' => $fine,
+            'supremacy' => $supremacy,
+        ] = $state->me->track->moveConflictPawn($power, $state->enemy->track);
+
+        if ($fine > 0) {
+            $state->enemy->treasure->add(-$fine);
+        }
+
+        if ($supremacy) {
+            $state->over(Victory::Military, $state->me->name);
+        }
     }
 }
